895ea21de21fbfb43a316a484a51cf7569bf2a7d
Georgie <georgiex@gmail.com>
2025-11-28 17:05:11 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    a = 3
+    b = 4
+    print("Hello World") 
+    print( a * b)
+
+if __name__ == '__main__':
+    main()
---
+++ temperature-units.py
@@ -0,0 +1,19 @@
+def welcome():
+    welcome = "This is a Farenheit to Celcius converter. Enter the number followed by the unit symbol e.g. 11F or 40C"
+    
+
+def input():
+    user_temp = input("Please enter your temperature: ")
+
+def validate_input():
+    if len(user_temp)
+
+
+
+
+def main():
+    welcome()
+    user_temp()
+    
+    
+    
